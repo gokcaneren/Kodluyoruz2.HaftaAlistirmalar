0bdd9f21e903c628c03ceb690a35192e79192697
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-03-02 15:57:41 +0300

    Add files via upload

---
+++ Kodluyoruz2.HaftaAlistirma2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodluyoruz2.HaftaAlistirma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 3 ==0 &&  i % 7 ==0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

9c599be86e2bc6a8f6a35fbe8008c5f78a442cc9
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-03-02 15:59:16 +0300

    Add files via upload

---
+++ Kodluyoruz2.HaftaAlistirma5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodluyoruz2.HaftaAlistirma5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 10;
+            int toplam = 0;
+            for (int i = 0; i < sayac; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz:", i+1);
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                toplam = toplam + sayi;
+            }
+            Console.WriteLine(toplam);
+
+            Console.ReadKey();
+        }
+    }
+}
